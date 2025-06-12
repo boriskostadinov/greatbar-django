d27d1ab70e0a988a536b4df35fd8e233e3fb70e6
Boris Kostadinov <boriskostadinof@gmail.com>
2025-06-12 14:20:32 +0300

    changing UI and style

--- store/views.py
+++ store/views.py
@@ -19,13 +19,13 @@ def store(request, category_slug=None):
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
-        paginator = Paginator(products, 6)  # 6 is number of products shown on one page
+        paginator = Paginator(products, 12)  # 6 is number of products shown on one page
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
         product_count = products.count()
     else:
         products = Product.objects.all().filter(is_available=True).order_by('id')
-        paginator = Paginator(products, 3)  # 6 is number of products shown on one page
+        paginator = Paginator(products, 12)  # 6 is number of products shown on one page
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
         product_count = products.count()
